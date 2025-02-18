00d48d369e719bbafc1e3602243d4b90ddf12ab0
taek2222 <oot309@daum.net>
2025-02-18 19:09:41 +0900

    test(AttendanceWarningTypeTest): 테스트 리팩토링
    - 출결 여러 상황에 알맞는 파라미터 테스트 형태로 변경했습니다.

--- src/test/java/attendance/domain/AttendanceWarningTypeTest.java
+++ src/test/java/attendance/domain/AttendanceWarningTypeTest.java
@@ -1,24 +1,43 @@
 package attendance.domain;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.stream.Stream;
+
+import static attendance.domain.AttendanceWarningType.COUNSELING;
+import static attendance.domain.AttendanceWarningType.EXPULSION;
+import static attendance.domain.AttendanceWarningType.NONE;
+import static attendance.domain.AttendanceWarningType.WARNING;
+import static attendance.domain.AttendanceWarningType.find;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class AttendanceWarningTypeTest {
 
-    @Test
+    @ParameterizedTest
+    @MethodSource()
     @DisplayName("출결 상황에 따라 알맞은 경고를 반환한다.")
-    void 출결_상황에_따라_알맞은_경고를_반환한다() {
-        // given
-        int absence = 3;
-        int late = 2;
-
+    void 출결_상황에_따라_알맞은_경고를_반환한다(int absence, int late, AttendanceWarningType type) {
         // when
-        AttendanceWarningType result = AttendanceWarningType.find(absence, late);
+        AttendanceWarningType result = find(absence, late);
 
         // then
         assertThat(result)
-                .isEqualTo(AttendanceWarningType.COUNSELING);
+                .isEqualTo(type);
+    }
+
+    static Stream<Arguments> 출결_상황에_따라_알맞은_경고를_반환한다() {
+        return Stream.of(
+                Arguments.of(0, 0, NONE),
+                Arguments.of(0, 5, NONE),
+                Arguments.of(2, 0, WARNING),
+                Arguments.of(0, 8, WARNING),
+                Arguments.of(3, 0, COUNSELING),
+                Arguments.of(0, 15, COUNSELING),
+                Arguments.of(6, 0, EXPULSION),
+                Arguments.of(0, 18, EXPULSION)
+        );
     }
 }
