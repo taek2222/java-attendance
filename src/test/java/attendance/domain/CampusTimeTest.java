0b5a74cae099bbcb920be69c5e35d4a40119c186
KJungW <sagakkb0707@naver.com>
2025-02-18 21:21:17 +0900

    test(CampusTimeTest): 테스트 리펙토링
    - "캠퍼스 운영시간이 아니라면 예외를 발생한다."
      테스트 케이스 추가

--- src/test/java/attendance/domain/CampusTimeTest.java
+++ src/test/java/attendance/domain/CampusTimeTest.java
@@ -1,17 +1,23 @@
 package attendance.domain;
 
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
 import java.time.LocalTime;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class CampusTimeTest {
 
-    @Test
+    @ParameterizedTest
+    @CsvSource({
+            "07:59:00",
+            "23:01:00",
+    })
     @DisplayName("캠퍼스 운영시간이 아니라면 예외를 발생한다.")
-    void 캠퍼스_운영시간이_아니라면_예외를_발생한다() {
-        Assertions.assertThatIllegalArgumentException()
-                .isThrownBy(() -> CampusTime.validateOperationTime(LocalTime.of(7, 59)))
+    void 캠퍼스_운영시간이_아니라면_예외를_발생한다(LocalTime target) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> CampusTime.validateOperationTime(target))
                 .withMessage("[ERROR] 캠퍼스 운영시간이 아닙니다.");
     }
 
