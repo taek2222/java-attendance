2b047b5f24850613b86245c1be5cbcf2c7b970f6
KJungW <sagakkb0707@naver.com>
2025-02-19 14:22:04 +0900

    feat(Attendance): 디폴트 출석 기록 생성
    - 입력된 날짜에 해당하는 디폴트 출석 기록을 생성한다.

--- src/main/java/attendance/domain/Attendance.java
+++ src/main/java/attendance/domain/Attendance.java
@@ -1,5 +1,16 @@
 package attendance.domain;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
 public class Attendance {
 
+    private final LocalDateTime dateTime;
+    private final AttendanceStatusType status;
+
+    public Attendance(LocalDate date) {
+        this.dateTime = LocalDateTime.of(date, LocalTime.MIN);
+        this.status = AttendanceStatusType.EXPULSION;
+    }
 }
