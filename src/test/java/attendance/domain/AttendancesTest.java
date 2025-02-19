191c84b8485a370085a1ea13f69f6bf55142ae1b
KJungW <sagakkb0707@naver.com>
2025-02-19 16:17:08 +0900

    test(AttendancesTest): 테스트 수정
    - 모킹 날짜 생성기를 주입해서 테스트하도록 수정한다.

--- src/test/java/attendance/domain/AttendancesTest.java
+++ src/test/java/attendance/domain/AttendancesTest.java
@@ -1,10 +1,20 @@
 package attendance.domain;
 
+import attendance.utility.DateGenerator;
+import java.time.LocalDate;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class AttendancesTest {
+
+    DateGenerator dateGenerator;
+
+    @BeforeEach
+    void beforeEach() {
+        dateGenerator = new MockingDateGenerator();
+    }
 
     @DisplayName("크루가 추가될때 기본 출석이 생성된다")
     @Test
@@ -12,13 +22,21 @@ class AttendancesTest {
         // given
         String name = "랜디";
         Holiday holiday = new Holiday();
-        Attendances attendances = new Attendances(holiday);
+        Attendances attendances = new Attendances(holiday, dateGenerator);
 
         // when
         attendances.addCrew(name);
 
         // then
         Assertions.assertThat(attendances.findCrewAttendance(name).size())
-                .isEqualTo(20);
+                .isEqualTo(22);
+    }
+
+    static class MockingDateGenerator implements DateGenerator {
+
+        @Override
+        public LocalDate now() {
+            return LocalDate.of(2024, 12, 5);
+        }
     }
 }
