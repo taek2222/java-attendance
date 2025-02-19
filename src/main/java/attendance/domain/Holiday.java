3cb056dc28bb920525920714840cadae8d98847e
taek2222 <oot309@daum.net>
2025-02-19 13:54:30 +0900

    feat(Holiday): 공휴일 추가하는 기능 추가
    - 확장성을 고려해 공휴일을 추가하는 메서드를 추가했다.
    
    - 공휴일도 휴일로 판정될 수 있도록 리팩토링 했다.

--- src/main/java/attendance/domain/Holiday.java
+++ src/main/java/attendance/domain/Holiday.java
@@ -3,6 +3,7 @@ package attendance.domain;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.format.TextStyle;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
@@ -11,10 +12,17 @@ public class Holiday {
     private static final DayOfWeek SATURDAY = DayOfWeek.SATURDAY;
     private static final DayOfWeek SUNDAY = DayOfWeek.SUNDAY;
 
-    private List<LocalDate> publicHolidays; // todo : 공휴일 네이밍 의문
+    private final List<LocalDate> publicHolidays = new ArrayList<>(); // todo : 공휴일 네이밍 의문, 객체 생성 과정 의문
+
+    public void addHoliday(LocalDate holiday) {
+        if (publicHolidays.contains(holiday)) {
+            throw new IllegalArgumentException("[ERROR] 이미 추가된 휴일입니다.");
+        }
+        publicHolidays.add(holiday);
+    }
 
     public void validateHoliday(LocalDate date) {
-        if (date.getDayOfWeek() == SATURDAY || date.getDayOfWeek() == SUNDAY) {
+        if (date.getDayOfWeek() == SATURDAY || date.getDayOfWeek() == SUNDAY || publicHolidays.contains(date)) {
             String formatted = String.format("[ERROR] %d월 %d일 %s은 등교일이 아닙니다.",
                     date.getMonthValue(),
                     date.getDayOfMonth(),
