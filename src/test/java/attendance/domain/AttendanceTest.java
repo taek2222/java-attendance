a25cc0bd16cd0f404b15470fef4296fae1443001
KJungW <sagakkb0707@naver.com>
2025-02-19 14:22:49 +0900

    test(AttendanceTest): 테스트 추가
    - 기본 날짜와 시간으로 출석을 생성할 수 있다

--- src/test/java/attendance/domain/AttendanceTest.java
+++ src/test/java/attendance/domain/AttendanceTest.java
@@ -2,6 +2,7 @@ package attendance.domain;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
 
+import java.time.LocalDate;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,7 +11,7 @@ class AttendanceTest {
     @DisplayName("기본 날짜와 시간으로 출석을 생성할 수 있다.")
     @Test
     void 기본_날짜와_시간으로_출석을_생성할_수_있다() {
-        assertThatCode(Attendance::new)
+        assertThatCode(() -> new Attendance(LocalDate.MIN))
                 .doesNotThrowAnyException();
     }
 }
