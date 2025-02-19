ad1f2e49621570947a7893e984c306f3c2245a87
KJungW <sagakkb0707@naver.com>
2025-02-19 18:06:04 +0900

    test(AttendancesTest): 테스트 추가
    - 출석을 추가할 수 있다
    - 해당 날짜의 출석을 찾을 수 있다

---
+++ src/main/java/attendance/domain/Attendances.java
@@ -0,0 +1,21 @@
+package attendance.domain;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Attendances {
+
+    private final List<Attendance> attendances = new ArrayList<>();
+
+    public void add(Attendance attendance) {
+        attendances.add(attendance);
+    }
+
+    public Attendance find(LocalDate date) {
+        return attendances.stream()
+                .filter(attendance -> attendance.isEqualDate(date))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("[ERROR] 서버 오류가 발생했습니다."));
+    }
+}
