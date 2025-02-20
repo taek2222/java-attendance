9b2915d0a659eb6e2b7bf36d183a0bcf5da41239
taek2222 <oot309@daum.net>
2025-02-20 19:57:57 +0900

    refactor(DateTimeParser): 메시지 상수 분리 및 Final 키워드 삽입

--- src/main/java/attendance/utility/DateTimeParser.java
+++ src/main/java/attendance/utility/DateTimeParser.java
@@ -9,33 +9,37 @@ import java.time.format.DateTimeParseException;
 
 public final class DateTimeParser {
 
-    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+    private static final String PATTERN = "yyyy-MM-dd HH:mm";
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern(PATTERN);
+
+    private static final String INPUT_DATETIME_MESSAGE = "[ERROR] 잘못된 날짜와 시간 형식입니다.";
+    private static final String INPUT_DATE_MESSAGE = "[ERROR] 잘못된 날짜 형식입니다.";
+    private static final String INPUT_TIME_MESSAGE = "[ERROR] 잘못된 시간 형식입니다.";
 
     private DateTimeParser() {
     }
 
-    public static LocalDateTime parseDateTime(String input) {
+    public static LocalDateTime parseDateTime(final String input) {
         try {
             return LocalDateTime.parse(input, FORMATTER);
         } catch (DateTimeParseException e) {
-            throw new IllegalArgumentException("[ERROR] 잘못된 날짜와 시간 형식입니다.");
+            throw new IllegalArgumentException(INPUT_DATETIME_MESSAGE);
         }
     }
 
-    public static LocalDate parseDateByDay(LocalDate date, int day) {
+    public static LocalDate parseDateByDay(final LocalDate date, final int day) {
         try {
             return date.withDayOfMonth(day);
         } catch (DateTimeException e) {
-            throw new IllegalArgumentException("[ERROR] 잘못된 날짜 형식입니다.");
+            throw new IllegalArgumentException(INPUT_DATE_MESSAGE);
         }
     }
 
-    public static LocalTime parseTime(String input) {
+    public static LocalTime parseTime(final String input) {
         try {
             return LocalTime.parse(input);
         } catch (DateTimeParseException e) {
-            throw new IllegalArgumentException("[ERROR] 잘못된 시간 형식입니다.");
+            throw new IllegalArgumentException(INPUT_TIME_MESSAGE);
         }
     }
-
 }
