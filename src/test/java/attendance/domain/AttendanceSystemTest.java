97bcd3b13dc1d143b228907131c365d5b8429af0
taek2222 <oot309@daum.net>
2025-02-22 14:26:34 +0900

    test(AttendanceSystemTest): 테스트 리팩토링
    - 파라미터 테스트로 여러 시간의 출석을 확인했다.

--- src/test/java/attendance/domain/AttendanceSystemTest.java
+++ src/test/java/attendance/domain/AttendanceSystemTest.java
@@ -1,44 +1,55 @@
 package attendance.domain;
 
 import attendance.utility.DateGenerator;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class AttendanceSystemTest {
 
-    @DisplayName("크루의 출석 데이터를 저장한다.")
-    @Test
-    void 크루의_출석_데이터를_저장한다() {
-        // given
-        DateGenerator dateGenerator = new MockingDateGenerator();
-        AttendanceManager attendanceManager = new AttendanceManager(
-                new Holiday(),
-                dateGenerator
-        );
+    private DateGenerator dateGenerator;
+    private AttendanceManager attendanceManager;
+    private AttendanceSystem attendanceSystem;
 
+    @BeforeEach
+    void setup() {
+        dateGenerator = new MockingDateGenerator();
+        attendanceManager = new AttendanceManager(new Holiday(), dateGenerator);
+        attendanceSystem = new AttendanceSystem(attendanceManager);
+    }
+
+    @ParameterizedTest(name = "출석 시간: {0} | 출석 상황 결과 : {1}")
+    @MethodSource
+    void 크루의_출석_데이터를_저장한다(LocalTime time, AttendanceStatusType expected) {
+        // given
         String nickname = "이든";
+        LocalDate nowDate = dateGenerator.now();
 
         attendanceManager.addCrew(nickname);
-        AttendanceSystem attendanceSystem = new AttendanceSystem(attendanceManager);
-
-        LocalDate nowDate = dateGenerator.now();
-        LocalDateTime givenDateTime = LocalDateTime.of(nowDate, LocalTime.of(10, 6));
+        LocalDateTime dateTime = LocalDateTime.of(nowDate, time);
 
         // when
-        Attendance result = attendanceSystem.processAttendanceCheck(givenDateTime, nickname);
+        Attendance result = attendanceSystem.processAttendanceCheck(dateTime, nickname);
 
         // then
-        assertThat(result.getDateTime())
-                .isEqualTo(givenDateTime);
+        assertThat(result.getDateTime()).isEqualTo(dateTime);
+        assertThat(result.getStatus()).isEqualTo(expected);
+    }
 
-        assertThat(result.getStatus())
-                .isEqualTo(AttendanceStatusType.LATE);
+    static Stream<Arguments> 크루의_출석_데이터를_저장한다() {
+        return Stream.of(
+                Arguments.of(LocalTime.of(10, 5), AttendanceStatusType.ATTENDANCE),
+                Arguments.of(LocalTime.of(10, 30), AttendanceStatusType.LATE),
+                Arguments.of(LocalTime.MAX, AttendanceStatusType.EXPULSION)
+        );
     }
 
     private static class MockingDateGenerator implements DateGenerator {
